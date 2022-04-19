8cfd965a2b4054440f9fe2e3a6b699b79a9c9cde
zhangzeyu007 <zhangzeyu1234@gmail.com>
2022-04-19 15:21:29 +0800

    feat: css配置文件

---
+++ packages/theme-chalk/gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// gulp 配置文件
+
+const { series, src, dest } = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cssmin = require('gulp-cssmin');
+
+// 将 scss 编译成 css 并压缩，最后输出到 ./lib 目录下
+function compile() {
+  return src('./src/*.scss')
+    .pipe(sass.sync())
+    .pipe(autoprefixer({
+      browsers: ['ie > 9', 'last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cssmin())
+    .pipe(dest('./lib'));
+}
+
+// 拷贝 ./src/fonts 到 ./lib/fonts
+function copyfont() {
+  return src('./src/fonts/**')
+    .pipe(cssmin())
+    .pipe(dest('./lib/fonts'));
+}
+
+exports.build = series(compile, copyfont);
