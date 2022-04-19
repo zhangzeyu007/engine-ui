b66eeea64f42b1750d78d1e96a4bd02388db9346
zhangzeyu007 <zhangzeyu1234@gmail.com>
2022-04-19 16:31:37 +0800

    feat: 配置

---
+++ examples/types/index.d.ts
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Author: 张泽雨
+ * @Date: 2022-04-19 16:25:36
+ * @LastEditors: 张泽雨
+ * @LastEditTime: 2022-04-19 16:25:37
+ * @FilePath: \engine-ui\examples\types\index.d.ts
+ */
+export * from './element-ui'
+
+import * as ElementUI from './element-ui'
+export default ElementUI
